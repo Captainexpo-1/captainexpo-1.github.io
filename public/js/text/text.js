aae3599050164a69bce822c57664f60be51a6a55
Captainexpo-1 <captainexpo@gmail.com>
2025-04-07 18:44:51 -0400

    Updates!

--- public/js/text/text.js
+++ public/js/text/text.js
@@ -5,7 +5,8 @@ const textCtx = boidsCanvas.getContext('2d');
 let textCanvasWidth = textCanvas.width;
 let textCanvasHeight = textCanvas.height;
 const fontSize = 12; // Size of each '0' character
-const randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+const randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+!@#$%^&*()_+[]{}|;:,.<>?';
+const randomChar = () => randomChars[Math.floor(Math.random() * randomChars.length)];
 
 function lerp(a, b, t) {
     return a + (b - a) * t;
@@ -15,20 +16,22 @@ function randomScalingFunction() {
     const m = Math.random()
     return m**3;
 }
+let textLastFrameTime = performance.now();
+let deltaTime = 0;
 
 class FallingChar {
     constructor(x, y, z) {
         this.x = x;
         this.y = y;
         this.z = z;
-        this.char = String.fromCharCode(Math.floor(Math.random()*(0x4FFF - 0x4E00) + 0x4E00));
+        this.char = randomChar();
     }
 
     update() {
-        this.y += this.z / textCanvasHeight;
+        this.y += (this.z / textCanvasHeight) * deltaTime * 0.1;
         if (this.y > 1) {
             this.y = 0;
-            this.char = String.fromCharCode(Math.floor(Math.random()*(0x4FFF - 0x4E00) + 0x4E00));
+            this.char = randomChar();
         }
     }
 
@@ -57,6 +60,10 @@ function drawText() {
     textCtx.fillStyle = 'rgba(0, 0, 0, 0.1)';  
     textCtx.fillRect(0, 0, textCanvasWidth, textCanvasHeight);
     
+    // Calculate delta time
+    const now = performance.now();
+    deltaTime = now - textLastFrameTime;
+    textLastFrameTime = now;
 
     CHARS.forEach((char) => {
         char.update();
